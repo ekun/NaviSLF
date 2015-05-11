3ae1ff607f033b9e9e3428b4d977ad81b36a5d55
Marius Nedal Glittum <ekunamatata@gmail.com>
2015-05-11 08:51:50 +0200

    Update navislf.tamper.js

--- navislf.tamper.js
+++ navislf.tamper.js
@@ -4,7 +4,7 @@
 // @homepage    https://github.com/ekun/NaviSLF
 // @downloadURL https://raw.github.com/ekun/NaviToggl/master/navislf.tamper.js
 // @updateURL   https://raw.github.com/ekun/NaviToggl/master/navislf.tamper.js
-// @version    0.9.3
+// @version    0.9.4
 // @description  Imports SLF-bugzilla hours into Naviwep
 // @match      https://naviwep.steria.no/NaviWEB/*
 // @copyright  2014+, Marius Bækken Glittum
